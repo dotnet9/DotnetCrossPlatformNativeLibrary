94051ef5b8185f919c89924ebd71f1af9b326373
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-05-07 17:35:10 +0800

    update;

--- publish-AvaloniaEncryptedCSharpFileTest-win-x64-aot.bat
+++ publish-AvaloniaEncryptedCSharpFileTest-win-x64-aot.bat
@@ -3,6 +3,5 @@
 if exist "publish\avalonia-aot" rmdir /s /q "publish\avalonia-aot"
 
 dotnet publish "src\AvaloniaEncryptedCSharpFileTest\AvaloniaEncryptedCSharpFileTest.csproj" -c Release -f net10.0-windows -r win-x64 --self-contained true -o "publish\avalonia-aot\win-x64\AvaloniaEncryptedCSharpFileTest" /p:PublishAot=true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugType=none /p:DebugSymbols=false
-dotnet publish "src\AvaloniaEncryptedCSharpFileTest\AvaloniaEncryptedCSharpFileTest.csproj" -c Release -f net10.0 -r linux-x64 --self-contained true -o "publish\avalonia-aot\linux-x64\AvaloniaEncryptedCSharpFileTest" /p:PublishAot=false /p:PublishReadyToRun=false /p:PublishSingleFile=true /p:PublishTrimmed=false /p:IncludeNativeLibrariesForSelfExtract=true /p:DebugType=none /p:DebugSymbols=false
 
 for /r "publish\avalonia-aot" %%f in (*.pdb *.dbg *.xml *.r2rmap) do del /q "%%f"
